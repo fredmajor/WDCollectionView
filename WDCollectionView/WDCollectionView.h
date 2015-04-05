ecc96d7d9a08e4782d75ef90a594b43439461ede
Fred <major.freddy@yahoo.com>
2015-04-05 14:17:10 +0200

    Initial Commit

---
+++ WDCollectionView/WDCollectionView.h
@@ -0,0 +1,19 @@
+//
+//  WDCollectionView.h
+//  WDCollectionView
+//
+//  Created by Fred on 05/04/15.
+//  Copyright (c) 2015 wd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for WDCollectionView.
+FOUNDATION_EXPORT double WDCollectionViewVersionNumber;
+
+//! Project version string for WDCollectionView.
+FOUNDATION_EXPORT const unsigned char WDCollectionViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WDCollectionView/PublicHeader.h>
+
+
